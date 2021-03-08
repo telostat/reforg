397d55fde7768fddfba01ccdb547e604ec8148e0
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2021-03-08 11:14:59 +0800

    chore: update Reforg program and error definitions

--- src/System/Process/Reforg/Internal/Class.hs
+++ src/System/Process/Reforg/Internal/Class.hs
@@ -1,3 +1,5 @@
+-- | Definitions for monadic Reforg program definitions.
+--
 {-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
@@ -7,11 +9,13 @@
 
 module System.Process.Reforg.Internal.Class where
 
-import Colog                                (HasLog(..), LogAction, Message)
-import Control.Monad.Except                 (ExceptT(..), MonadError, runExceptT)
-import Control.Monad.IO.Class               (MonadIO)
-import Control.Monad.Reader                 (MonadReader, ReaderT(..), asks)
-import System.Process.Reforg.Internal.Types (Spec)
+import           Colog                  (HasLog(..), LogAction, Message)
+import           Control.Monad.Except   (ExceptT(..), MonadError, runExceptT)
+import           Control.Monad.IO.Class (MonadIO)
+import           Control.Monad.Reader   (MonadReader, ReaderT(..))
+import qualified Data.HashMap.Strict    as HM
+import qualified Data.Text              as T
+import           Path                   (Abs, File, Path)
 
 
 -- | Reforg program MTS definition.
@@ -28,36 +32,32 @@ runReforg env app = runReaderT (runExceptT (unReforg app)) env
 -- | Encoding for possible errors we may encounter while executing Reforg
 -- programs.
 data ReforgError =
-    ReforgErrorSpec String
-  | ReforgErrorIO String
+    ReforgErrorIO String
+  | ReforgErrorNoMatch (Path Abs File)
   | ReforgErrorProcess String
+  | ReforgErrorRegex String
+  | ReforgErrorSpec String
   | ReforgErrorUnknown String
   deriving (Show)
 
 
 -- | Provides Reforg program environment.
 data Env m = Env
-  { envSpec      :: !Spec
+  { envEnvars    :: !(HM.HashMap T.Text T.Text)
+  , envParams    :: !(HM.HashMap T.Text T.Text)
+  , envRevars    :: !(HM.HashMap T.Text T.Text)
+  , envFsvars    :: !(HM.HashMap T.Text T.Text)
   , envDryrun    :: !Bool
   , envLogAction :: !(LogAction m Message)  -- ^ Logger action implementation
   }
 
 
+-- | 'HasLog' instance for 'Env'.
 instance HasLog (Env m) Message m where
-    getLogAction :: Env m -> LogAction m Message
-    getLogAction = envLogAction
-    {-# INLINE getLogAction #-}
+  getLogAction :: Env m -> LogAction m Message
+  getLogAction = envLogAction
+  {-# INLINE getLogAction #-}
 
-    setLogAction :: LogAction m Message -> Env m -> Env m
-    setLogAction newLogAction env = env { envLogAction = newLogAction }
-    {-# INLINE setLogAction #-}
-
-
--- | Retrieves specification from the environment.
-askSpec :: MonadReader (Env m) m => m Spec
-askSpec = asks envSpec
-
-
--- | Retrieves dry-run indicator from the environment.
-askDryrun :: MonadReader (Env m) m => m Bool
-askDryrun = asks envDryrun
+  setLogAction :: LogAction m Message -> Env m -> Env m
+  setLogAction newLogAction env = env { envLogAction = newLogAction }
+  {-# INLINE setLogAction #-}
